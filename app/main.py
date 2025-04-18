7d0f9df32d7068c5445949c28528209538022bfe
kennethus <jnadolfo@up.edu.ph>
2025-04-18 22:59:14 +0800

    Updated CORS allow origins

--- app/main.py
+++ app/main.py
@@ -20,10 +20,15 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:5173'],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://127.0.0.1:5173"
+    ],
     allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*']
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 def convert_to_numeric(value):
