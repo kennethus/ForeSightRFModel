be7f9a2560f131047b3b6038f8661ff193bced61
kennethus <jnadolfo@up.edu.ph>
2025-05-01 13:34:05 +0800

    Added frontend URL in CORS

--- app/main.py
+++ app/main.py
@@ -25,7 +25,8 @@ app.add_middleware(
         "http://localhost:5173",
         "https://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://127.0.0.1:5173"
+        "https://127.0.0.1:5173",
+        "https://fore-sight-eight.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
